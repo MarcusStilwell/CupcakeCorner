a476959d24b388208b0ee6d7a5de08cba42be4c1
MarcusStilwell <marcusstilwell@icloud.com>
2021-11-01 13:39:33 -0700

    Added order screen and toggles for various special requests. Created order object to pass order data throughout app

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -11,25 +11,46 @@ struct ContentView: View {
     @State private var username = ""
     @State private var email = ""
     @State private var password = ""
+    @ObservedObject var order = Order()
     
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-                TextField("Password", text: $password)
-            }
-            
-            Section {
-                Button ("Create account") {
-                    print("Creating account...")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
                 }
             }
-            .disabled(disableForm)
+            .navigationTitle("Cupcake Corner")
         }
     }
 }
